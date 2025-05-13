5f327f3d5fed72a13ab499056ccca3702dc12a70
Jaydeep Bhattacharya <vedlogic1@ligtwayfinance.com>
2025-05-13 16:51:31 +0530

    interview Questions and pdf added

--- p2-path-modules/index.js
+++ p2-path-modules/index.js
@@ -10,5 +10,7 @@ console.log("Extension Name",path.extname(__filename))
 
 const jointPath = path.join("/users","document","node", "project");
 console.log("Joint Path", jointPath)
+
+//It wil gives us resolved path mean if you'r in /home/alex so it will give you /home/alex/users/document/node/project in my case (I'm using window) It will give me: D:\POCs\BE\BasicToPro\p2-path-modules\user\document\node\project
 const resolvePath = path.resolve("user","document","node","project")
 console.log("Resolve path",resolvePath)
